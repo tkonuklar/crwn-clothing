ce33ac3eefe5cdf80e27354fc230281d2c3385b4
tugcekonuklar <tugce.konuklar@infinitec.solutions>
2019-11-28 14:40:42 +0100

    broken our homepage into smaller components, finished styling or manu item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import './homepage.component'
-import { HomePage } from './homepage.component';
+
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <HomePage/>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
